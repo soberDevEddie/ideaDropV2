57890574b5c5b7c7291a66b5b908d06afd8c4b71
Eddie <soberDevEddie@Alexas-MacBook-Air.local>
2026-01-07 14:46:38 -0600

    working on app. left off on MockREST API section for IdeaDrop

---
+++ src/routes/__root.tsx
@@ -0,0 +1,37 @@
+import { Outlet, createRootRoute, HeadContent } from '@tanstack/react-router';
+import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools';
+import { TanStackDevtools } from '@tanstack/react-devtools';
+
+export const Route = createRootRoute({
+  head: () => ({
+    meta: [
+      {
+        name: 'description',
+        content:
+          'Share, explore and build on the best startup ideas and side hustles',
+      },
+      ,
+      {
+        title: 'IdeaDrop - Your Idea Hub',
+      },
+    ],
+  }),
+
+  component: () => (
+    <>
+      <HeadContent />
+      <Outlet />
+      <TanStackDevtools
+        config={{
+          position: 'bottom-right',
+        }}
+        plugins={[
+          {
+            name: 'Tanstack Router',
+            render: <TanStackRouterDevtoolsPanel />,
+          },
+        ]}
+      />
+    </>
+  ),
+});
