4c4c922691b08b34b48471353446567fd60aecac
EddieO23 <eocampo52@gmail.com>
2026-01-13 08:33:59 -0600

    added Ideas to /ideas page

--- src/routes/ideas/index.tsx
+++ src/routes/ideas/index.tsx
@@ -1,14 +1,57 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, Link } from '@tanstack/react-router';
+import { queryOptions, useSuspenseQuery } from '@tanstack/react-query';
+
+import type { Idea } from '@ideahub/types';
+import api from '@/lib/axios';
+
+const fetchIdeas = async (): Promise<Idea[]> => {
+  const res = await api.get('/ideas');
+  return res.data;
+};
+
+const ideasQueryOptions = () =>
+  queryOptions({
+    queryKey: ['ideas'],
+    queryFn: () => fetchIdeas(),
+  });
 
 export const Route = createFileRoute('/ideas/')({
-  head : () => ({
-    meta: [
-      {title: 'IdeaHub - Browse Ideas'}
-    ]
+  head: () => ({
+    meta: [{ title: 'IdeaHub - Browse Ideas' }],
   }),
   component: IdeasPage,
-})
+  loader: async ({ context: { queryClient } }) => {
+    return queryClient.ensureQueryData(ideasQueryOptions());
+  },
+});
 
 function IdeasPage() {
-  return <div>Hello "/ideas/"!</div>
+  const { data: ideas } = useSuspenseQuery(ideasQueryOptions());
+  // console.log(ideas);
+
+  return (
+    <div className='p-4'>
+      <h1 className='text-2xl font-bold mb-4'>Ideas</h1>
+      <ul className='grid grid-cols-1 sm:grid-cols-2 gap-6'>
+        {ideas.map((idea) => (
+          <li
+            key={idea.id}
+            className='border border-gray-300 p-4 rounded shadow bg-white flex flex-col justify-between'
+          >
+            <div>
+              <h2 className='text-lg font-semibold'>{idea.title}</h2>
+            </div>
+            <p className='mt-2 text-gray-700'>{idea.summary}</p>
+            <Link
+              to='/ideas/$ideaId'
+              params={{ ideaId: idea.id.toString() }}
+              className='mt-4 text-white rounded px-4 py-2 bg-blue-600 hover:underline hover:bg-blue-700 text-center'
+            >
+              View Details
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
