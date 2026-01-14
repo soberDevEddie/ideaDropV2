7b9323b6307634d5201f4d5d5bae0c1e14efbba6
EddieO23 <eocampo52@gmail.com>
2026-01-14 17:13:10 -0600

    added 404 notFound

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -2,6 +2,7 @@ import {
   Outlet,
   createRootRouteWithContext,
   HeadContent,
+  Link,
 } from '@tanstack/react-router';
 import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools';
 import { TanStackDevtools } from '@tanstack/react-devtools';
@@ -28,6 +29,7 @@ export const Route = createRootRouteWithContext<RouterContext>()({
   }),
 
   component: RootLayout,
+  notFoundComponent: NotFound,
 });
 
 function RootLayout() {
@@ -53,4 +55,23 @@ function RootLayout() {
       />
     </div>
   );
+}
+
+
+function NotFound () {
+  return (
+    <div className="flex flex-col items-center justify-center text-center py-20">
+      <h1 className='text-4xl font-bold text-gray-800 mb-4'>
+        404 - Page Not Found
+      </h1>
+      <p className='text-lg text-gray-600 mb-6'>
+        Oooops the page you are looking for does not exist.
+      </p>
+      <Link to="/">
+        <button className="bg-blue-700 hover:bg-blue-700 text-white font-bold py-2 px-6 rounded transition duration-500">
+          Go Home
+        </button>
+      </Link>
+    </div>
+  )
 }
