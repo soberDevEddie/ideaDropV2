560d6a4e9044fef5e9b3f494c56720bb917bc829
EddieO23 <eocampo52@gmail.com>
2026-01-15 14:42:19 -0600

    started working on New Ideas Page. Set up state for form submission. Need jsx markup for form. Got the link set up for for the markup.

--- src/routes/ideas/new/index.tsx
+++ src/routes/ideas/new/index.tsx
@@ -1,9 +1,26 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, useNavigate } from '@tanstack/react-router';
+import { useState } from 'react';
+import type { Idea } from '@/types';
 
 export const Route = createFileRoute('/ideas/new/')({
   component: NewIdeaPage,
-})
+});
 
 function NewIdeaPage() {
-  return <div>Hello "/ideas/new/"!</div>
+  const navigate = useNavigate();
+  const [title, setTitle] = useState('');
+  const [summary, setSummary] = useState('');
+  const [description, setDescription] = useState('');
+  const [tags, setTags] = useState('');
+
+  return (
+    <div className='space-y-6'>
+      <h1 className='text-3xl font-bold mb-6'>Create New Idea</h1>
+
+      <form className='space-y-4'>
+
+        
+      </form>
+    </div>
+  );
 }
