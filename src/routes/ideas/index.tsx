78c90c0583e69c0be81b3a31e016783584da2781
EddieO23 <eocampo52@gmail.com>
2026-01-15 14:37:13 -0600

    worked on added cusom classes for Home page and Ideas Page for custom classes for the buttons/links.

--- src/routes/ideas/index.tsx
+++ src/routes/ideas/index.tsx
@@ -1,7 +1,8 @@
-import { createFileRoute, Link } from '@tanstack/react-router';
+import { createFileRoute } from '@tanstack/react-router';
 import { queryOptions, useSuspenseQuery } from '@tanstack/react-query';
 
 import { fetchIdeas } from '@/api/ideas';
+import IdeaCard from '@/components/IdeaCard';
 
 const ideasQueryOptions = () =>
   queryOptions({
@@ -26,26 +27,11 @@ function IdeasPage() {
   return (
     <div className='p-4'>
       <h1 className='text-2xl font-bold mb-4'>Ideas</h1>
-      <ul className='grid grid-cols-1 sm:grid-cols-2 gap-6'>
+      <div className='grid grid-cols-1 sm:grid-cols-2 gap-6'>
         {ideas.map((idea) => (
-          <li
-            key={idea.id}
-            className='border border-gray-300 p-4 rounded shadow bg-white flex flex-col justify-between'
-          >
-            <div>
-              <h2 className='text-lg font-semibold'>{idea.title}</h2>
-            </div>
-            <p className='mt-2 text-gray-700'>{idea.summary}</p>
-            <Link
-              to='/ideas/$ideaId'
-              params={{ ideaId: idea.id.toString() }}
-              className='mt-4 text-white rounded px-4 py-2 bg-blue-600 hover:underline hover:bg-blue-700 text-center'
-            >
-              View Details
-            </Link>
-          </li>
+          <IdeaCard key={idea.id} idea={idea} />
         ))}
-      </ul>
+      </div>
     </div>
   );
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,6 +3,7 @@ import { Lightbulb } from 'lucide-react';
 
 import { fetchIdeas } from '@/api/ideas';
 import { queryOptions, useSuspenseQuery } from '@tanstack/react-query';
+import IdeaCard from '@/components/IdeaCard';
 
 const ideasQueryOptions = () =>
   queryOptions({
@@ -17,10 +18,10 @@ export const Route = createFileRoute('/')({
 });
 
 function HomePage() {
-  const {data: ideas} = useSuspenseQuery(ideasQueryOptions());
+  const { data: ideas } = useSuspenseQuery(ideasQueryOptions());
   // console.log(ideas);
 
-  const latestIdeas = ideas.slice(0, 3)
+  const latestIdeas = ideas.slice(0, 3);
 
   return (
     <div className='flex flex-col md:flex-row items-start justify-between gap-10 p-6 text-blue-600'>
@@ -38,20 +39,11 @@ function HomePage() {
         <h2 className='text-2xl font-semibold mb-4 text-gray-800'>
           Latest Ideas
         </h2>
-        <ul className='space-y-6'>
+        <div className='space-y-6'>
           {latestIdeas.map((idea) => (
-            <li key={idea.id} className='border border-gray-300 rounded-lg shadow p-4 bg-white'>
-            <h3 className='text-lg font-bold text-gray-900'>{idea.title}</h3>
-            <p className='text-gray-600 mb-2'>
-              {idea.summary}
-            </p>
-            <Link to='/ideas/$ideaId' params={{ideaId: idea.id.toString()}}
-            className='text-blue-600 hover:underline'>
-              Read more →
-            </Link>
-          </li>
+            <IdeaCard key={idea.id} idea={idea} button={false} />
           ))}
-        </ul>
+        </div>
         <div className='mt-6'>
           <Link
             to='/ideas'
