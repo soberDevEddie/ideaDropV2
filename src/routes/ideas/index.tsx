f8f35a28e6f44095b56d5dbba040d7566def3da9
EddieO23 <eocampo52@gmail.com>
2026-01-14 18:33:20 -0600

    worked on Ideas home page. Fetching ideas from query to home page to showcase ideas

--- src/routes/ideas/index.tsx
+++ src/routes/ideas/index.tsx
@@ -1,18 +1,12 @@
 import { createFileRoute, Link } from '@tanstack/react-router';
 import { queryOptions, useSuspenseQuery } from '@tanstack/react-query';
 
-import type { Idea } from '@/types';
-import api from '@/lib/axios';
-
-const fetchIdeas = async (): Promise<Idea[]> => {
-  const res = await api.get('/ideas');
-  return res.data;
-};
+import { fetchIdeas } from '@/api/ideas';
 
 const ideasQueryOptions = () =>
   queryOptions({
     queryKey: ['ideas'],
-    queryFn: () => fetchIdeas(),
+    queryFn: fetchIdeas,
   });
 
 export const Route = createFileRoute('/ideas/')({
